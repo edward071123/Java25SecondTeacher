88e75f9cf908fd1f448805497c79dbc6af4f0884
Soma0617 <soma0928153146@gmail.com>
2025-04-23 13:33:19 +0800

    上傳

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! I am Edward No. 1232!");
 
-        System.out.println("Hello, World! I am Edward No. 17test!");
+        System.out.println("Hello, World! I am Edward No. 17test2223!");
     }
 }
