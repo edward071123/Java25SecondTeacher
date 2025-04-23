def406ee2549b558d8dcb2be0bacdb5ff353c8c9
java <rabbit88823@gmail.com>
2025-04-23 11:23:39 +0800

    v1 Hello World

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("New Helloworld!");
+        System.out.println("New Hello World!");
     }
 }
