65d9c551c77acd6b9da32db5d94c7f1701bce59e
yoyo2050123 <yoyo2050123@gmail.com>
2025-04-23 11:23:33 +0800

    testdev2

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! I am Edward No. 123!");
+        System.out.println("Hello123456, World! I am Edward No. 123!");
 
         System.out.println("Hello, World! I am Edward No. 17!");
     }
