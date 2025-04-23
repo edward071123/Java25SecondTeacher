26fae5554bac586e25892d7e474f5617e1089df2
EdwardTest01 <glen0711@hotmail.com>
2025-04-23 13:23:11 +0800

    Update on github #2

--- src/App.java
+++ src/App.java
@@ -1,7 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! I am Edward No. 12345!");
-
-        System.out.println("Hello, World! I am Edward No. 17!");
+        System.out.println("Hello, World!");
     }
 }
