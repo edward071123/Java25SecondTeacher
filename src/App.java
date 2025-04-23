4a0822817fc760fdc59c585d834f704d22b56ff1
Glen <glen0711@hotmail>
2025-04-24 02:47:15 +0800

    feat: test

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World123!");
+        System.out.println("Hello, World!");
     }
 }
