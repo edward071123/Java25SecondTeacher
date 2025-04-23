b3d6d0d437bb70855f98faa856ae73b8df6483b1
marksman456 <hank6831511@yahoo.com.tw>
2025-04-23 11:17:51 +0800

    test 學生

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! I am Edward No. 145456!");
 
-        System.out.println("Hello, World! I am Edward No. 17!");
+        System.out.println("Hello, World! I am Edward No. 23!");
     }
 }
