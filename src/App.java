a1963d46a7ecb5f22fecf52d46ab4bbe8f5525cd
EDWARDCHUNG96C8\edward <leonardo071123@gmal.com>
2025-04-24 02:16:04 +0800

    feat: init

--- src/App.java
+++ src/App.java
@@ -2,8 +2,5 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! I am Edward No. 1!");
 
-        System.out.println("Hello, World! I am Edward No. 17!");
-
-         System.out.println("Hello, World! I am Edward No. 18!");
     }
 }
--- src/App2.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        // 印出 hello world印出 hello world
-        System.out.println("Hello, World!");
-    }
-}
